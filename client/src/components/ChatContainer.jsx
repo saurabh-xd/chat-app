3f5b0694bd5a14dd0b3aad47e0faa13f26ebb9b5
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-08-16 19:37:17 +0530

    error fix

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import assets from '../assets/assets'
 
-const ChatContainer = () => {
+const ChatContainer = ({selectedUser, setSelectedUser}) => {
   return (
-    <div>ChatContainer</div>
+    <div>
+      <div className='flex items-center gap-3 py-3  mx-4 border-b border-stone-500'>
+        <img src={assets.profile_martin} className="w-8 rounded-full" />
+        <p className='flex-1 text-lg text-white flex items-center gap-2'>
+          Martin Johnson 
+          <span className='w-2 h-2 rounded-full bg-green-500'></span>
+        </p>
+        <img onClick={()=> setSelectedUser(null) }
+        src={assets.arrow_icon} className="md:hidden max-w-7" />
+      </div>
+    </div>
   )
 }
 
