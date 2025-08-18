989224a4a9555c92b1e5a94236750bf8dffea3f0
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-08-18 19:59:02 +0530

    chat container done

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import assets, { messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../lib/utils'
 
 const ChatContainer = ({selectedUser, setSelectedUser}) => {
+
+  const scrollEnd = useRef()
+
+  useEffect(()=>{
+    if(scrollEnd.current){
+      scrollEnd.current.scrollIntoView({behavior: "smooth"})
+    }
+  },[])
+
+
   return selectedUser ? (
     <div className='h-full overflow-scroll relative backdrop-blur-lg'>
       <div className='flex items-center gap-3 py-3  mx-4 border-b border-stone-500'>
@@ -26,12 +37,25 @@ const ChatContainer = ({selectedUser, setSelectedUser}) => {
           )}
           <div className='text-center text-xs'>
             <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' ? assets.avatar_icon : assets.profile_martin} className="w-7 rounded-full" />
-            <p className='text-gray-500'>{msg.createdAt}</p>
+            <p className='text-gray-500'>{formatMessageTime(msg.createdAt) }</p>
 
           </div>
 
         </div>
       ))}
+
+      <div ref={scrollEnd}></div>
+    </div>
+
+    <div className='absolute bottom-0 left-0 right-0 flex items-center gap-3 p-3'>
+      <div className='flex-1 flex items-center bg-gray-100/12 px-3 rounded-full'>
+        <input type="text" placeholder='send a message' className='flex-1 text-sm p-3 border-none rounded-lg outline-none text-white placeholder-gray-400'/>
+        <input type="file" id='image' accept='image/png, image/jpeg' hidden />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" className='w-5 mr-2 cursor-pointer' />
+        </label>
+      </div>
+      <img src={assets.send_button} className="w-7 cursor-pointer" />
 
     </div>
 
