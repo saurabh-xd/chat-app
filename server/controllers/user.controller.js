71c73e35bfbf30eb4c1ac4336239daede8607550
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-08-29 17:42:12 +0530

    Add user profile update functionality

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.model.js";
 import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js"
 
 
 //Signup new user
@@ -64,5 +65,40 @@ export const login = async (req, res)=>{
           console.log(error.message);
         res.json({success: false, message: "failed to log in"})
         
+    }
+}
+
+// user auth controller
+export const checkAuth = (req, res)=>{
+    res.json({success: true, user: req.user});
+}
+
+//controller to update user profile details
+export const updateProfile = async (req, res)=>{
+    try{
+        const { profilePic, bio, fullName } = req.body;
+
+        const userId = req.user._id;
+        let updatedUser;
+
+        if (!profilePic) {
+            updatedUser = await User.findByIdAndUpdate(userId, {bio, fullName},
+                {new: true}
+            );
+        } else {
+            const upload = await cloudinary.uploader.upload(profilePic);
+
+            updatedUser = await User.findByIdAndUpdate(userId, {profilePic: upload.secure_url, bio, fullName},
+                {new: true}
+            )
+        }
+
+        res.json({success: true, user: updatedUser})
+
+    } catch(error){
+        console.log(error.message);
+        
+
+        res.json({success: false, message: error.message})
     }
 }
