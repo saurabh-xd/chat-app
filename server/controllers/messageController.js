9ab6a3675aa3e4598ca8bdd9700466cc05ef0d25
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-08-30 18:21:00 +0530

    implement message retrieval and marking as seen functionality

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,4 +1,4 @@
-import Message from "../models/Message.js";
+import Message from "../models/Message.model.js";
 import User from "../models/User.model.js";
 
 
@@ -32,4 +32,43 @@ export const getUsersForSidebar = async (req, res)=>{
         
         
     }
+}
+
+// Get all messages for selected user
+export const getMessages = async (req, res)=>{
+    try {
+        const { id: selectedUserId } = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId: selectedUserId},
+                {senderId: selectedUserId, receiverId: myId},
+            ]
+        })
+        await Message.updateMany({senderId: selectedUserId, receiverId: myId},
+            {seen: true}
+        );
+
+        res.json({success: true, messages})
+        
+    } catch (error) {
+        console.log(error.Message);
+        res.json({success: false, message: error.Message})
+    }
+}
+
+// api to mark message as seen using message id
+export const markMessageAsSeen = async (req, res)=>{
+
+    try {
+        const { id } = req.params;
+        await Message.findByIdAndUpdate(id, {seen: true})
+        res.json({success: true})
+        
+    } catch (error) {
+        console.log(error.Message);
+        res.json({success: false, message: error.Message})
+    }
+
 }
