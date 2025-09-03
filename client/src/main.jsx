d302bd754aa91adf869097d0e3904faa173f9f77
saurabh garkoti <saurabhgarkotiii@gmail.com>
2025-09-03 20:36:57 +0530

    Add ChatContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from '../context/AuthContext.jsx'
+import { ChatProvider } from '../context/ChatContext.jsx'
 
 
 
@@ -11,7 +12,10 @@ createRoot(document.getElementById('root')).render(
   
   <BrowserRouter>
   <AuthProvider>
-     <App />
+    <ChatProvider>
+ <App />
+    </ChatProvider>
+    
   </AuthProvider>
  
   </BrowserRouter>
